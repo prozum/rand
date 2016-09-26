e02a078ac3b671a963a62aa654c602ed58b4b81b
Kasper Steensig <ksje14@student.aau.dk>
2016-09-26 23:15:36 +0200

    io: added some thoughts anout an interface

--- src/lib/io.h
+++ src/lib/io.h
@@ -1,4 +1,29 @@
 #ifndef RAND_IO_H
 #define RAND_IO_H
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+
+typedef enum pin_mode_e {
+    OUTPUT  = 0,
+    INPUT   = 1
+} pin_mode_t;
+
+// DDRx is data direction register
+// PORTx is the port state
+
+// analog IO example:
+//      http://www.avrfreaks.net/forum/tut-c-newbies-guide-avr-adc?name=PNphpBB2&file=viewtopic&t=56429
+
+// set a pin to either input or output
+void io_set_pin(uint8_t port, uint8_t pin, pin_mode_t);
+//void io_clear(uint8_t port, uint8_t pin);
+
+void io_digital_write(uint8_t pin, char out);
+char io_digital_read(uint8_t pin);
+
+void io_analog_write(uint8_t pin, uint16_t);
+uint16_t io_analog_read(uint8_t pin);
+
 #endif //RAND_IO_H
