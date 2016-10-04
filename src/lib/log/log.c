237279374f3ccd6e2a209311906341e8315445a7
Morell <tmorel14@student.aau.dk>
2016-10-04 11:54:28 +0200

    Added framework for logging - remeber to initialize it by calling toggle_logging

---
+++ src/lib/log/log.c
@@ -0,0 +1,41 @@
+#include "log.h"
+#include <string.h>
+#include "io.h"
+
+//Prefix defines:
+#define WARNING_PREFIX "W: "
+#define ERROR_PREFIX "E: "
+#define MSG_PREFIX "M: "
+
+//logging_level defines which class(es) of errors that should be logged
+level logging_level;
+
+/*Toggles logging at the given level - must be called during start-up of drone!*/
+void toggle_logging(level lvl) {
+    //Write project-name and version to buffer
+    const char *ver = strcat(", version: ", MAJOR_VERSION);
+    serial_write_string(USB_TX, (sval_t *)(PROJECT_NAME, ver));
+
+    logging_level = lvl;
+}
+
+/*Logs a message directly to serial output*/
+void LOG(char *msg) {
+    if(logging_level == LOG_ALL) {
+        serial_write_string(USB_TX, (sval_t *)strcat(MSG_PREFIX, msg));
+    }
+}
+
+/*Logs a warning directly to serial output*/
+void LOG_WARNING(char msg[]) {
+    if(logging_level > LOG_ONLY_ERRORS) {
+        serial_write_string(USB_TX, (sval_t * )strcat(WARNING_PREFIX, msg));
+    }
+}
+
+/*Logs an error directly to serial output.*/
+void LOG_ERROR(char msg[]) {
+    if(logging_level >= LOG_ONLY_ERRORS) {
+        serial_write_string(USB_TX, (sval_t *) strcat(ERROR_PREFIX, msg));
+    }
+}
