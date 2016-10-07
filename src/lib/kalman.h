67d1388eb46235f411e94feacb8bb215bee7f19d
Morell <tmorel14@student.aau.dk>
2016-10-07 13:38:12 +0200

    Started implementing kalman filter - partly defined header.

--- src/lib/kalman.h
+++ src/lib/kalman.h
@@ -1,15 +1,29 @@
 #include <stdint.h>
 #include "io.h"
 
-typedef static struct {
-    double q; //process noise covariance
-    double r; //measurement noise covariance
-    double x; //value
-    double p; //estimation error covariance
-    double k; //kalman gain
+typedef struct {
+    const uint8_t size;
+
+    const double *A;
+    const double *B;
+    const double *C;
+    const double *R;
+
+    double u_k; //Control signal
+    double z_k; //Current observation
+    double *G_k; //kalman gain matrix
+    double *P_k; //prediction error matrix
+
 } kalman_state;
 
-kalman_state kalman_init (double q, double r, double p, double intial_value);
-
+void kalman_init (kalman_state state, const uint8_t size);
+void kalman_setA (kalman_state state, const double *values);
+void kalman_setB (kalman_state state, const double *values);
+void kalman_setC (kalman_state state, const double *values);
+void kalman_setR (kalman_state state, const double *values);
 void kalman_update(kalman_state* state, double measurement);
+
+kalman_state IR_kalman;
+kalman_state dist_kalman;
+kalman_state fc_kalman;
 
