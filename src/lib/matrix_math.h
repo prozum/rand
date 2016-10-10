244425172ac1025e42a6f26501581ea09110d533
Morell <tmorel14@student.aau.dk>
2016-10-10 09:01:24 +0200

    Small change to matrix-mult function

--- src/lib/matrix_math.h
+++ src/lib/matrix_math.h
@@ -5,7 +5,7 @@
 #ifndef RAND_MATRIX_MATH_H
 #define RAND_MATRIX_MATH_H
 
-void mult_mat_mat(double *m1, double *m2);
+void mult_mat_mat(double *m1, double *m2, uint8_t mat_size);
 void mult_mat_vec(double *m, double *v);
 void mat_T(double *m);
 void mat_inv(double *m);
--- src/lib/matrix_math/matrix_math.c
+++ src/lib/matrix_math/matrix_math.c
@@ -1,13 +1,7 @@
 #include "matrix_math.h"
 
-void mult_mat_mat(double m1[][], double m2[][]) {
-    int m1_size = sizeof(m1) / sizeof(double);
-    int m2_size = sizeof(m2) / sizeof(double);
-
-    if(m1_size != m2_size) {
-        LOG_WARNING(SENDER_BOARD, "Cannot multiply matrices of different size.");
-        return;
-    }
+void mult_mat_mat(double m1[][], double m2[][], uint8_t mat_size) {
+    
 }
 
 void mult_mat_vec(double *m, double *v) {
