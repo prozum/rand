04720ec557c83bbfd9c93f7a0823b2f6661f707f
Morell <tmorel14@student.aau.dk>
2016-10-12 13:23:48 +0200

    Started on the Kalman matrix approach

--- src/lib/kalman.h
+++ src/lib/kalman.h
@@ -1,23 +1,28 @@
 #include <stdint.h>
 #include "io.h"
 
-typedef struct {
-    const uint8_t size;
+typedef struct kalman_state_t {
+    uint8_t size;
     log_sender source_components;
 
     double *A; //Weight of previous measurement in next estimate
-    const double *B; //Weight of control-input in next estimate
-    const double *C; //Noise scale constant
-    const double *R; //variance of sensor, i.e. how much a measurement tends to differ from the actual
+    double *B; //Weight of control-input in next estimate
+    double *C; //Noise scale constant
+    double *R; //variance of sensor, i.e. how much a measurement tends to differ from the actual
+    double *Q; //Covariance of process noise
 
-    double u_k; //Control signal
+    double *u_k; //Control signal
     double z_k; //Current observation
     double *G_k; //kalman gain matrix
     double *P_k; //prediction error matrix
 
+    double *x_k; //State variable
+
 } kalman_state;
 
 void kalman_init (kalman_state *state, const uint8_t size, log_sender component);
+void kalman_destroy(kalman_state *state);
+
 void kalman_setA (kalman_state *state, const double *values);
 void kalman_setB (kalman_state *state, const double *values);
 void kalman_setC (kalman_state *state, const double *values);
--- src/lib/kalman/kalman.c
+++ src/lib/kalman/kalman.c
@@ -1,5 +1,13 @@
 #include "kalman.h"
 #include <stdlib.h>
+#include "matrix_math.h"
+
+void check_for_null_matrix(kalman_state *state) {
+    if(!state->A || !state->B || !state->C || !state->R || !state->G_k || !state->P_k)
+        LOG_ERROR(state->source_components, "Kalman-state failed to allocate space for a matrix.");
+
+    //It's quite bad, if the matrices are not malloc'ed - do we need some sort of abort flight function?
+}
 
 void kalman_init (kalman_state *state, uint8_t size, log_sender component)
 {
@@ -16,43 +24,114 @@ void kalman_init (kalman_state *state, uint8_t size, log_sender component)
     state->G_k = malloc(tot_size);
     state->P_k = malloc(tot_size);
 
+    //State and control-signal
+    state->u_k = malloc(size);
+    state->x_k = malloc(size);
+
     state->source_components = component;
+    state->size = size;
 
     check_for_null_matrix(state);
 }
 
-void check_for_null_matrix(kalman_state *state) {
-    if(!state->A || !state->B || !state->C || !state->R || !state->G_k || !state->P_k)
-        LOG_ERROR(state->source_components, "Kalman-state failed to allocate space for a matrix.");
+void kalman_destroy(kalman_state *state) {
+    free(state->A);
+    free(state->B);
+    free(state->C);
+    free(state->R);
+
+    free(state->G_k);
+    free(state->P_k);
+    free(state->x_k);
+
+    LOG(state->source_components, "This function nullifies the struct - do not use it again!");
+
+    free(state);
 }
 
 void kalman_setA (kalman_state *state, const double *values)
 {
-   
+    uint8_t tot_size = state->size * state->size;
+
+    if(state->A) {
+        int i = 0;
+
+        for (i; i < tot_size; ++i) {
+            state->A[i] = values[i];
+        }
+    }
 }
 
 void kalman_setB (kalman_state *state, const double *values)
 {
+    uint8_t tot_size = state->size * state->size;
 
+    if(state->B) {
+        int i = 0;
+
+        for (i; i < tot_size; ++i) {
+            state->B[i] = values[i];
+        }
+    }
 }
 
 void kalman_setC (kalman_state *state, const double *values)
 {
+    uint8_t tot_size = state->size * state->size;
 
+    if(state->C) {
+        int i = 0;
+
+        for (i; i < tot_size; ++i) {
+            state->C[i] = values[i];
+        }
+    }
 }
 
 void kalman_setR (kalman_state *state, const double *values)
 {
+    uint8_t tot_size = state->size * state->size;
 
+    if(state->R) {
+        int i = 0;
+
+        for (i; i < tot_size; ++i) {
+            state->R[i] = values[i];
+        }
+    }
 }
 
-void kalman_filter (kalman_state* state, double measurement)
+void kalman_predict (kalman_state* state, double measurement)
 {
+    //state->z_k = add_vec_vec(mult_mat_vec(state->C, state->x_k), state->v_k);
 
+    //x_k = A*x_k + B*u_k
+    state->x_k = add_vec_vec(mult_mat_vec(state->A, state->x_k), mult_mat_vec(state->B, state->u_k));
+    //P_k = A * P_(k-1) * A^T
+    state->P_k = add_mat_mat(mult_mat_mat(mult_mat_mat(state->A, state->P_k, state->size),
+                                          mat_T(state->A), state->size));
 }
 
 void kalman_update(kalman_state* state, double measurement)
 {
+    //C^T
+    double *C_trans = mat_T(state->C);
+
+    //G_k = P_k * C^T * (C * P_k * C^T + R)^-1
+    state->G_k = mult_mat_mat(
+            mult_mat_mat(state->P_k, C_trans, state->size),
+                mat_inv(add_mat_mat(
+                        mult_mat_mat(state->C, mult_mat_mat(state->P_k, C_trans, state->size),
+                        state->R), state->size)),
+            state->size);
+
+    //x_k = x_k + G_k (z_k - C * x_k)
+    state->x_k = add_vec_vec(state->x_k, mult_mat_vec(state->G_k,
+                                                      sub_vec_vec(state->z_k, mult_mat_vec(state->C, state->x_k))));
+
+    //P_k = (I - G_k * C) * P_k
+    state->P_k = mult_mat_mat(sub_mat_mat(I, mult_mat_mat(state->G_k, state->C)), state->P_k);
+
 
 }
 
