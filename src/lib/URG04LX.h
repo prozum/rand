6f03ea5f896abdba9e753b27088d98a73c4363ad
GoatSnail <gedesneglen@gmail.com>
2016-10-12 15:26:44 +0200

    - Added serial_read_string function - Wrote documentation for all serial communication's function prototypes.

---
+++ src/lib/URG04LX.h
@@ -0,0 +1,8 @@
+#include "io.h"
+
+#ifndef RAND_URG04LX_H
+#define RAND_URG04LX_H
+
+#endif //RAND_URG04LX_H
+
+
--- test/unit/io/io_test.py
+++ test/unit/io/io_test.py
@@ -6,3 +6,4 @@ import unittest
 class IODigitalWrite(unittest.TestCase):
     def write1_expect1(self):
         return 1
+
