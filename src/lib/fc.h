d46609de62ad4d7ddcaa2b08a54512e7e8ed53ff
Kasper <ksje14@student.aau.dk>
2016-10-31 09:43:58 +0100

    fc: added code for navigating the drone

--- src/lib/fc.h
+++ src/lib/fc.h
@@ -3,16 +3,34 @@
 
 #include <stdint.h>
 
-static const uint16_t MIN_FC_PWM = 1000;
-static const uint16_t MID_FC_PWM = 1500;
-static const uint16_t MAX_FC_PWM = 2000;
+const uint16_t MIN_FC_DUTY = 1250;
+const uint16_t MID_FC_DUTY = 1500;
+const uint16_t MAX_FC_DUTY = 1750;
 
-void rotate_z_axis(uint16_t angle);
+// low, medium and high stick positions
+static const uint16_t STICK_L = 1000;
+static const uint16_t STICK_M = 1500;
+static const uint16_t STICK_H = 2000;
 
-void move_x_axis(int8_t d);
+static void set_yaw(uint16_t d);
+static void set_pitch(uint16_t d);
+static void set_roll(uint16_t d);
+static void set_throttle(uint16_t d);
 
-void move_y_axis(int8_t d);
+void init_fc();
+void arm_fc();
+void disarm_fc();
 
-void move_z_axis(int8_t d);
+void rotate_left();
+void rotate_right();
+void rotate_stop();
+
+void move_left();
+void move_right();
+void move_forward();
+void move_back();
+void move_up();
+void move_down();
+void move_stop();
 
 #endif //RAND_FC_H
