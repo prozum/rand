edcac95a17cb0fb51ae073c8e885ef6e16b0ab48
Morell <tmorel14@student.aau.dk>
2016-11-08 09:48:32 +0100

    Mock: Analog, log: debug flag
    Implemented io-mock function for analog_read and analog_write. Also added code for only logging errors, when the debug-flag is set.

--- src/lib/ir/ir.c
+++ src/lib/ir/ir.c
@@ -19,8 +19,8 @@ void IR_callibrate(float z_0)
     read_top_IR();
     read_bottom_IR();
 
-    kalman_calibrate(&IR_top_state, 100); // the 100 is Z_0, needs to be changed
-    kalman_calibrate(&IR_bottom_state, 100); // the 100 is Z_0, needs to be changed
+    kalman_calibrate(&IR_top_state, 100); // the 100 is z, needs to be changed
+    kalman_calibrate(&IR_bottom_state, 100); // the 100 is z, needs to be changed
 }
 
 float read_top_IR() {
