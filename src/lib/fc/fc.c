1966e430cf4193ec368e7e14cb361373f12608ed
Kasper <ksje14@student.aau.dk>
2016-11-15 13:24:14 +0100

    added scheduler code and wrote fc api

--- src/lib/fc/fc.c
+++ src/lib/fc/fc.c
@@ -1,101 +1,77 @@
 #include "fc/fc.h"
 
-/* private interface */
-static void set_yaw(uint16_t d)
+void init_fc(uint16_t ms)
 {
-
+    MIN_FC_DUTY = ms;
+    MID_FC_DUTY = (ms * 1.5);
+    MAX_FC_DUTY = (ms * 2);
 }
 
-static void set_pitch(uint16_t d)
+void set_arm()
 {
-
+    yaw = MAX_FC_DUTY;
+    throttle = MIN_FC_DUTY;
+    pitch = MID_FC_DUTY;
+    roll = MID_FC_DUTY;
 }
 
-static void set_roll(uint16_t d)
+void set_disarm()
 {
-
-}
-
-static void set_throttle(uint16_t d)
-{
-
-}
-
-void init_fc()
-{
-
-}
-
-// when pitch and roll are set to medium,
-// throttle is set to low and yaw is set to high
-// then the naze32 is armed
-void arm_fc()
-{
-    set_pitch(STICK_M);
-    set_roll(STICK_M);
-    set_throttle(STICK_L);
-    set_yaw(STICK_H);
-}
-
-// when pitch and roll are set to medium,
-// throttle is set to low and yaw is set to low
-// then the naze32 is disarmed
-void disarm_fc()
-{
-    set_pitch(STICK_M);
-    set_roll(STICK_M);
-    set_throttle(STICK_L);
-    set_yaw(STICK_L);
+    yaw = MIN_FC_DUTY;
+    throttle = MIN_FC_DUTY;
+    pitch = MID_FC_DUTY;
+    roll = MID_FC_DUTY;
 }
 
 void rotate_left()
 {
-    set_yaw(MIN_FC_DUTY);
+    yaw = MIN_FC_DUTY;
 }
 
 void rotate_right()
 {
-    set_yaw(MAX_FC_DUTY);
+    yaw = MAX_FC_DUTY;
 }
 
 void rotate_stop()
 {
-    set_yaw(MID_FC_DUTY);
+    yaw = MID_FC_DUTY;
 }
 
 void move_left()
 {
-    set_roll(MIN_FC_DUTY);
+    roll = MIN_FC_DUTY;
 }
 
 void move_right()
 {
-    set_roll(MAX_FC_DUTY);
+    roll = MAX_FC_DUTY;
 }
 
 void move_forward()
 {
-    set_pitch(MAX_FC_DUTY);
+    pitch = MAX_FC_DUTY;
 }
 
 void move_back()
 {
-    set_pitch(MIN_FC_DUTY);
+    pitch = MIN_FC_DUTY;
 }
 
 void move_up()
 {
-    set_throttle(MAX_FC_DUTY);
+    throttle = MAX_FC_DUTY;
 }
 
 void move_down()
 {
-    set_throttle(MIN_FC_DUTY);
+    throttle = MIN_FC_DUTY;
 }
 
 void move_stop()
 {
-    set_throttle(MID_FC_DUTY);
-    set_pitch(MID_FC_DUTY);
-    set_yaw(MID_FC_DUTY);
+    throttle = MID_FC_DUTY;
+    pitch = MID_FC_DUTY;
+    roll = MID_FC_DUTY;
+    yaw = MID_FC_DUTY;
 }
--- src/main.c
+++ src/main.c
@@ -1,37 +1,33 @@
 #include "task.h"
 
-void task_timer_setup()
-{
-    // disable interrupts
-    cli();
-
-    // reset all timer flags
-    TCCR1A = 0;
-    TCCR1B = 0;
-
-    // set prescaler of 256
-    TCCR1B |= (1 << CS12);
-
-    // enable interrupts
-    sei();
-}
-
-void task_arm_fc()
-{
-
-}
-
-void task_start_pulse()
-{
-
-}
-
-void task_stop_pulse()
-{
-
-}
-
 int main ()
 {
+    // initialize tasks
 
+    /* insert non-timer dependent tasks here */
+
+    task_init_fc();
+
+    // initialize timer dependent tasks
+    task_timer_setup();
+
+    task_arm_fc();
+
+    while (1)
+    {
+        task_pulse();
+
+        while(TCNT1 <= MINOR_CYCLE);
+
+        TCNT1 = 0;
+    }
 }
+
+/*
+ISR(TIMER1_OVF_vect)
+{
+    goto schedule_start;
+    return;
+}
+ */
+
