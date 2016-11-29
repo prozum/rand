1b2ae9c5057fa74b7076d3b240f22f4f4813485a
Kasper <ksje14@student.aau.dk>
2016-11-29 10:34:06 +0100

    added some new tasks

--- src/task.c
+++ src/task.c
@@ -1,5 +1,52 @@
 #include "task.h"
 
+void init_not_timed() {
+    // initialize tasks
+    sonar = sonar_init(SONAR_TRIGGER_PIN, SONAR_ECHO_PIN);
+    laser = laser_init(USB_RX);
+    top_ir = IR_init(IR_TOP_PIN);
+    bottom_ir = IR_init(IR_BOTTOM_PIN);
+    task_init_fc();
+    /*Maybe some error-checking here*/
+
+    float a[SENSOR_FILTERS];
+    a[LEFT_READING] = SIDE_a;
+    a[RIGHT_READING] = SIDE_a;
+    a[TOP_READING] = TOP_BOT_a;
+    a[BOTTOM_READING] = TOP_BOT_a;
+
+    float r[SENSOR_FILTERS];
+    r[LEFT_READING] = LASER_r;
+    r[RIGHT_READING] = LASER_r;
+    r[TOP_READING] = IR_r;
+    r[BOTTOM_READING] = IR_r;
+
+    float A[DATAFUSION_FILTERS];
+    A[FRONT_READING] = FRONT_a;
+
+    float R[DATAFUSION_FILTERS];
+    R[FRONT_READING] = LASER_r + SONAR_r / 2;
+
+    recent_position = positioning_init(a, r, A, R);
+
+    read_sonar(sonar);
+    IR_read(bottom_ir);
+    IR_read(top_ir);
+    //laser_read(laser);
+
+    float readings[SENSOR_FILTERS];
+    readings[LEFT_READING] = laser->left_value;
+    readings[RIGHT_READING] = laser->right_value;
+    readings[TOP_READING] = top_ir->value;
+    readings[BOTTOM_READING] = bottom_ir->value;
+
+    float df_readings[DATAFUSION_FILTERS];
+    df_readings[ZLASER] = laser->front_value;
+    df_readings[ZSONAR] = sonar->value;
+
+    positioning_calibrate(recent_position, readings, df_readings);
+}
+
 void task_timer_setup()
 {
     // disable interrupts
@@ -109,12 +156,23 @@ void task_pulse()
         digital_write(THROTTLE, LOW);
 }
 
-void task_update_movement()
+void task_navigation()
 {
 
 }
 
-void task_navigation()
+void task_read_laser()
 {
+    laser_read_dist(laser);
+}
 
+void task_read_ir()
+{
+    IR_read(bottom_ir);
+    IR_read(top_ir);
+}
+
+void task_read_sonar()
+{
+    read_sonar(sonar);
 }
